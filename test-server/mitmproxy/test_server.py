f28787e98545a28a657a842e69a3682485a8d1b8
Amol Bhave <ammubhave@gmail.com>
2017-08-17 19:28:44 -0400

    add more sites to test server

--- test-server/mitmproxy/test_server.py
+++ test-server/mitmproxy/test_server.py
@@ -2,9 +2,28 @@ import mitmproxy
 import re
 from mitmproxy.script import concurrent
 
-reddit_patterns = [r'https?:\/\/.*reddit\.com.*',
-                   r'https?:\/\/.*\.redditmedia\.com\/.*',
-                   r'https?:\/\/www\.redditstatic\.com\/.*']
+#reddit_patterns = [r'https?:\/\/.*reddit\.com.*',
+#                   r'https?:\/\/.*\.redditmedia\.com\/.*',
+#                   r'https?:\/\/www\.redditstatic\.com\/.*']
+#mitfcu_patterns = [r'https?:\/\/.*mitfcu\.org.*',
+#                   r'https?:\/\/.*mitfcu2\.org.*']
+
+patterns = [
+    ([r'https?:\/\/.*reddit\.com.*',
+      r'https?:\/\/.*\.redditmedia\.com\/.*',
+      r'https?:\/\/www\.redditstatic\.com\/.*'], 'reddit.com'),
+    ([r'https?:\/\/.*mitfcu\.org.*',
+      r'https?:\/\/.*mitfcu2\.org.*'], 'mitfcu.org'),
+    ([r'https?:\/\/.*yelp\.com.*',
+      r'https?:\/\/.*yelpcdn\.com.*'], 'yelp.com'),
+    ([r'https?:\/\/.*google\.com.*',
+      r'https?:\/\/.*googleusercontent\.com.*',
+      r'https?:\/\/.*gstatic\.com.*',
+      r'https?:\/\/.*googleapis\.com.*'], 'google.com'),
+    ([r'https?:\/\/.*speedtest\.net.*',
+      r'https?:\/\/.*beld\.net.*',
+      r'https?:\/\/.*cdnst\.net.*'], 'speedtest.net'),
+]
 
 @concurrent
 def request(flow):
@@ -14,12 +33,10 @@ def request(flow):
 def response(flow):
     url = flow.request.pretty_url
     print(flow.response.headers, flow.request.pretty_url)
-    for pattern in reddit_patterns:
-        if re.match(pattern, url):
-            flow.response.headers["Origin"] = 'reddit.com'
-            flow.response.headers.set_all("Origin-Pattern", [r'https?:\/\/.*reddit\.com.*',
-               r'https?:\/\/.*\.redditmedia\.com\/.*',
-               r'https?:\/\/www\.redditstatic\.com\/.*'])
-            print("matched")
-            return
+    for ps, origin in patterns:
+        for pattern in ps:
+            if re.match(pattern, url):
+                flow.response.headers["Origin"] = origin
+                flow.response.headers.set_all("Origin-Pattern", ps)
+                return
 
